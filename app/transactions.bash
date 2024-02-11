0f8802346ece0164a523016ac50433642ce649cf
Waltenne Carvalho <waltenne.carvalho@oobj.com.br>
2024-02-11 10:40:48 -0300

    Trying to fix canabrava

--- app/transactions.bash
+++ app/transactions.bash
@@ -1,37 +1,45 @@
 function handle_POST_transactions() {
   ID=${PARAMS["id"]}
+  if [ -z "$BODY" ]; then
+    RESPONSE="400 Bad Request: Corpo da solicitação ausente"
+    return
+  fi
+
   AMOUNT=$(echo "$BODY" | jq -r '.valor')
   TRANSACTION_TYPE=$(echo "$BODY" | jq -r '.tipo')
   DESCRIPTION=$(echo "$BODY" | jq -r '.descricao')
 
+  echo "$ID $AMOUNT $TRANSACTION_TYPE $DESCRIPTION"
+
   if [ "$TRANSACTION_TYPE" == "c" ]; then
     OPERATION="+"
-  else
+  elif [ "$TRANSACTION_TYPE" == "d" ]; then
     OPERATION="-"
+  else
+    RESPONSE="400 Bad Request: Tipo de transação inválido"
   fi
 
   if [ ! -z "$ID" ]; then
-    QUERY="
-INSERT INTO transactions (account_id, amount, description, transaction_type)
-VALUES ($ID, $AMOUNT, '$DESCRIPTION', '$TRANSACTION_TYPE');
+    # Construindo a consulta SQL
+    QUERY="BEGIN TRANSACTION;
+      INSERT INTO transactions (account_id, amount, description, transaction_type)
+      VALUES ($ID, $AMOUNT, '$DESCRIPTION', '$TRANSACTION_TYPE');
 
-UPDATE balances
-SET amount = amount $OPERATION $AMOUNT
-WHERE balances.account_id = $ID;
+      UPDATE balances
+      SET amount = amount $OPERATION $AMOUNT
+      WHERE balances.account_id = $ID;
 
-SELECT 
-  json_build_object(
-    'limite', accounts.limit_amount,
-    'saldo', balances.amount
-  )
-FROM accounts 
-LEFT JOIN balances ON balances.account_id = accounts.id
-WHERE account_id = $ID"
+      SELECT 
+        json_object('limite', accounts.limit_amount, 'saldo', balances.amount)
+      FROM accounts 
+      LEFT JOIN balances ON balances.account_id = accounts.id
+      WHERE account_id = $ID;
+    COMMIT;"
 
-    RESULT=`psql -t -h pgbouncer -U postgres -d postgres -p 6432 -c "$QUERY" | tr -d '[:space:]'` 
+    # Executando a consulta SQL
+    RESULT=$(echo "$QUERY" | sqlite3 "$DB_FILE")
 
     if [ ! -z "$RESULT" ]; then
-
       RESPONSE=$(cat views/bank_statement.jsonr | sed "s/{{data}}/$RESULT/")
     else
       RESPONSE=$(cat views/404.htmlr)
